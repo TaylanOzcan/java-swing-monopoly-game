bfc4ed64cba087c129c233bbca61baddb0fbc660
mohamedalhajar <mohamedalhajar@172.20.132.47>
2017-12-10 13:36:01 +0300

    Added Junit to the testing

--- ProjectMonopoly/src/Testing/BoardTesting.java
+++ ProjectMonopoly/src/Testing/BoardTesting.java
@@ -1,5 +1,11 @@
 package Testing;
-
-public class BoardTesting {
-
-}
+import org.junit.*;
+import domain.Board;
+	
+	public class BoardTesting {
+		public void BoardTestinProcedures () {
+			
+		Board b = new Board (null); //Class Board is being tested 
+		
+		}
+	}
